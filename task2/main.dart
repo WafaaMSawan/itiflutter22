639ca3b2532ac2835079a4d29f601eee6dac06d4
WafaaMSawan <112361602+WafaaMSawan@users.noreply.github.com>
2022-08-29 18:48:21 +0200

    Add files via upload

--- task2/main.dart
+++ task2/main.dart
@@ -1,1 +1,206 @@
+import 'package:flutter/material.dart';
 
+void main() {
+runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+const MyApp({Key? key}) : super(key: key);
+ @override
+Widget build(BuildContext context) {
+return MaterialApp(
+debugShowCheckedModeBanner: false,
+home: Scaffold(
+backgroundColor: Colors.white,
+appBar: AppBar(
+//title: const Text('Business Card'),
+leading: Icon(Icons.arrow_back_ios,color: Colors.black,),
+backgroundColor: Colors.amber,
+//foregroundColor: Colors.white,
+),
+body: Center(
+child: Column(
+mainAxisAlignment: MainAxisAlignment.start,
+children: [
+Container(
+height: 120,
+width: double.infinity,
+color: Colors.amber,
+child: Row(
+mainAxisAlignment: MainAxisAlignment.start,
+children: [
+SizedBox(width: 25,),
+Column(
+mainAxisAlignment: MainAxisAlignment.end,
+children: [
+Text(
+'Account Settings',
+style: TextStyle(
+color: Colors.black,
+fontSize: 25,
+fontWeight: FontWeight.bold,
+),
+),
+SizedBox(height: 15,),],),
+],
+),
+),
+SizedBox(height: 15,),
+CircleAvatar(
+backgroundImage: AssetImage('images/smile.png'),
+radius: 40,
+),
+Text(
+'wafaa mohamed',
+style: TextStyle(
+color: Colors.black,
+fontSize: 20,
+fontWeight: FontWeight.bold,
+),
+),
+SizedBox(height: 10,),
+Row(
+mainAxisAlignment: MainAxisAlignment.start,
+children: [
+SizedBox(width: 10,),
+Text(
+'Edit Profile',
+style: TextStyle(
+color: Colors.black54,
+fontSize: 15,
+fontWeight: FontWeight.bold,
+),
+),
+],
+),
+SizedBox(height: 10,),
+Row(
+mainAxisAlignment: MainAxisAlignment.start,
+children: [
+SizedBox(width: 10,),
+Column(
+mainAxisAlignment: MainAxisAlignment.end,
+children: [
+Text(
+'Number Phone',
+style: TextStyle(
+color: Colors.black,
+fontSize: 12,
+fontWeight: FontWeight.bold,
+),
+),
+SizedBox(height: 5,),
+Text(
+'01285955692',
+style: TextStyle(
+color: Colors.black54,
+fontSize: 12,
+fontWeight: FontWeight.bold,
+),
+),],),
+SizedBox(width: 260,),
+Icon(Icons.phone,color: Colors.black54,),
+],
+),
+Text(
+'______',
+style: TextStyle(
+color: Colors.black54,
+fontSize: 25,
+//fontWeight: FontWeight.bold,
+),
+),
+Row(
+mainAxisAlignment: MainAxisAlignment.start,
+children: [
+SizedBox(width: 10,),
+Column(
+mainAxisAlignment: MainAxisAlignment.end,
+children: [
+Text(
+'Email',
+style: TextStyle(
+color: Colors.black,
+fontSize: 12,
+fontWeight: FontWeight.bold,
+),
+),
+SizedBox(height: 5,),
+Text(
+'wafaa@gmail.com',
+style: TextStyle(
+color: Colors.black54,
+fontSize: 12,
+fontWeight: FontWeight.bold,
+),
+),],),
+SizedBox(width: 260,),
+Icon(Icons.email,color: Colors.black54,),
+],
+),
+Text(
+'______',
+style: TextStyle(
+color: Colors.black54,
+fontSize: 25,
+//fontWeight: FontWeight.bold,
+),
+),
+Row(
+mainAxisAlignment: MainAxisAlignment.start,
+children: [
+SizedBox(width: 10,),
+Column(
+mainAxisAlignment: MainAxisAlignment.end,
+children: [
+Text(
+'Password',
+style: TextStyle(
+color: Colors.black,
+fontSize: 12,
+fontWeight: FontWeight.bold,
+),
+),
+SizedBox(height: 5,),
+Text(
+'***',
+style: TextStyle(
+color: Colors.black54,
+fontSize: 12,
+fontWeight: FontWeight.bold,
+),
+),],),
+SizedBox(width: 300,),
+Icon(Icons.remove_red_eye,color: Colors.black54,),
+],
+),
+Text(
+'______',
+style: TextStyle(
+color: Colors.black54,
+fontSize: 25,
+//fontWeight: FontWeight.bold,
+),
+),
+Text(
+'Logout',
+style: TextStyle(
+color: Colors.red,
+fontSize: 25,
+fontWeight: FontWeight.bold,
+),
+),
+Text(
+'______',
+style: TextStyle(
+color: Colors.red,
+fontSize: 25,
+//fontWeight: FontWeight.bold,
+),
+),
+],
+),
+),
+),
+);
+}}
